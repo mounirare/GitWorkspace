b432d1c04a8d645d009d553f514ad353dc12e775
papidje <dseremoussa@gmail.com>
2015-02-03 13:22:17 +0100

    Classe ExtractData

---
+++ Simulation/src/com/metaheuristique/utils/ExtractData.java
@@ -0,0 +1,93 @@
+package com.metaheuristique.utils;
+
+import java.util.ArrayList;
+
+public class ExtractData {
+	Fichier file = new Fichier();
+	String directory = "Exemple/";
+	String fileName;
+	
+	public ArrayList<String> readBusStops(){
+		fileName = "Busstops.csv";
+		String stream;
+		
+		stream = file.lecture(directory+fileName);
+		
+		String[] streamTab = stream.split("\n");
+		
+		ArrayList<String> busStops = new ArrayList<String>();
+		for (int i = 1; i < streamTab.length; i++){
+			busStops.add(streamTab[i]);
+		}
+		
+		return busStops;
+	}
+	
+	public ArrayList<String> readFleet(){
+		fileName = "Fleet.csv";
+		String stream;
+		
+		stream = file.lecture(directory+fileName);
+		
+		String[] streamTab = stream.split("\n");
+		
+		ArrayList<String> busStops = new ArrayList<String>();
+		for (int i = 1; i < streamTab.length; i++) {
+			busStops.add(streamTab[i]);
+		}
+		
+		return busStops;
+	}
+	
+	public ArrayList<String> readDistTimeCoo(){
+		fileName = "DistanceTimesCoordinates.csv";
+		String stream;
+		
+		stream = file.lecture(directory+fileName);
+		
+		String[] streamTab = stream.split("\n");
+		
+		ArrayList<String> busStops = new ArrayList<String>();
+		for (int i = 1; i < streamTab.length; i++) {
+			busStops.add(streamTab[i]);
+		}
+		
+		return busStops;
+	}
+	
+	public ArrayList<String> readDistTimeCoa(){
+		fileName = "DistanceTimesData_COACHES.csv";
+		String stream;
+		
+		stream = file.lecture(directory+fileName);
+		
+		String[] streamTab = stream.split("\n");
+		
+		ArrayList<String> busStops = new ArrayList<String>();
+		for (int i = 1; i < streamTab.length; i++) {
+			busStops.add(streamTab[i]);
+		}
+		
+		return busStops;
+	}
+	
+	public ArrayList<String> readDistTimeShu(){
+		fileName = "DistanceTimesData_SHUTTLES.csv";
+		String stream;
+		
+		stream = file.lecture(directory+fileName);
+		
+		String[] streamTab = stream.split("\n");
+		
+		ArrayList<String> busStops = new ArrayList<String>();
+		for (int i = 1; i < streamTab.length; i++) {
+			busStops.add(streamTab[i]);
+		}
+		
+		return busStops;
+	}
+
+	public String[] spliter(String in){
+		return in.split(";");
+	}
+}
