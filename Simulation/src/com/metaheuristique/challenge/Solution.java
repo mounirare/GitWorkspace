03dbfde08a5dd6ecb02dbe8bfdce7d7bbcc2f20d
LAMeunier <meunier.laurenta@gmail.com>
2015-02-17 15:08:34 +0100

    Solution + ArrayList

---
+++ Simulation/src/com/metaheuristique/challenge/Solution.java
@@ -0,0 +1,33 @@
+package com.metaheuristique.challenge;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+	private ArrayList<Coach> coachList;
+	private ArrayList<Shuttle> shuttleList;
+	private double totalCost;
+	
+	public Solution(ArrayList<Coach> coachList, ArrayList<Shuttle> shuttleList){
+		this.coachList = coachList;
+		this.shuttleList = shuttleList;
+		totalCost = 0.0;
+	}
+	
+	public String coachPath(int indice){
+		String sol= new String();
+		Coach c = coachList.get(indice);
+		for(int i=0; i<c.getBusStopTraveled().size(); i++){
+			String stop = c.getIdCoach()+";"+i+1+";"+c.getBusStopTraveled().get(i)+";";
+			if(c.getBusStopTraveled().get(i).substring(0, 1) == "B")
+				stop = stop+"BUSSTOP";
+			else
+				stop = stop+"HUB";
+			sol = sol + stop + "\n";
+		}
+		return sol;
+		
+		
+		
+	}
+}
